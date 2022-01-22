2ad3c0a4feebd027c3761bb3a294f3eaec1b5865
rongtao7 <Rt337845946&>
2022-01-22 14:27:21 +0800

    最实用的设计模式专栏

---
+++ MyNotes-design/design-demo-chain/src/main/java/com/codertao/design/chain/DesignDemoChainApplication.java
@@ -0,0 +1,13 @@
+package com.codertao.design.chain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignDemoChainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignDemoChainApplication.class, args);
+    }
+
+}
---
+++ MyNotes-design/design-demo-chain/src/test/java/com/codertao/design/chain/DesignDemoChainApplicationTests.java
@@ -0,0 +1,22 @@
+package com.codertao.design.chain;
+
+import com.codertao.design.chain.case1.ProductService;
+import com.codertao.design.chain.case1.ProductVO;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+
+@SpringBootTest
+class DesignDemoChainApplicationTests {
+    @Resource
+    ProductService productService;
+
+    @Test
+    void contextLoads() {
+        ProductVO param = ProductVO.builder().skuId(123L).skuName("测试商品").imgPath("http://..").price(new BigDecimal(1)).stock(1).build();
+        productService.createProduct(param);
+    }
+
+}
