2e64a21dee7f8cdcfa69996bbce288d89c9298f5
rongtao7 <Rt337845946&>
2022-01-27 15:06:24 +0800

    format

--- MyNotes-design/design-demo-chain/src/test/java/com/codertao/design/chain/DesignDemoChainApplicationTests.java
+++ MyNotes-design/design-demo-chain/src/test/java/com/codertao/design/chain/DesignDemoChainApplicationTests.java
@@ -15,7 +15,13 @@ class DesignDemoChainApplicationTests {
 
     @Test
     void contextLoads() {
-        ProductVO param = ProductVO.builder().skuId(123L).skuName("测试商品").imgPath("http://..").price(new BigDecimal(1)).stock(1).build();
+        ProductVO param = ProductVO.builder()
+                .skuId(123L)
+                .skuName("测试商品")
+                .imgPath("http://..")
+                .price(new BigDecimal(1))
+                .stock(1)
+                .build();
         productService.createProduct(param);
     }
 
